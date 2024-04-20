da4982c902250a3f79fd4e525fd66749d33d2804
Nadigames <Nadigames@orange.fr>
2024-04-20 09:42:04 +0200

    Edit: async

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ function clearElement(element) {
   }
 }
 
-function linkListener(key){
+async function linkListener(key){
  if (nav_elements[key].url){
    const html = await getPage(nav_elements[key].url)
    console.log(html)
